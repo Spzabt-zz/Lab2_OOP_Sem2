f36846b4a3a72a5b7633078735ccffbfd2eb1f21
Szpabt-zz <stoida146@gmail.com>
2021-03-05 00:35:28 +0200

    Adding projects

---
+++ 1.3/Timer/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Timer
+{
+    public delegate string MyDelegate(string message, int index);
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 1.3/Timer/Timer.cs
@@ -0,0 +1,7 @@
+namespace Timer
+{
+    public class Timer
+    {
+        
+    }
+}
